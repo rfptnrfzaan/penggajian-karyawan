697c8b363c176763b4452c2ffafe16fd084e10b4
priyayidimas <priyayidimas@upi.edu>
2022-06-30 22:41:21 +0700

    Tax Calculator

--- app/Http/Requests/UpdateKaryawanRequest.php
+++ app/Http/Requests/UpdateKaryawanRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateKaryawanRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class UpdateKaryawanRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Auth::check();
     }
 
     /**
